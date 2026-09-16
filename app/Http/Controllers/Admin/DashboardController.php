9d1a4c3e6b02c66e77f1108a7bd636921cee8d15
andrea8980 <andrea@casaloda.it>
2026-09-16 15:00:26 +0200

    Add DashboardController with index and profile methods, and update routes

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        return "index amministratore";
+    }
+
+    public function profile()
+    {
+        return "pagina profilo backoffice";
+    }
 }
