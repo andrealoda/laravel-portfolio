360302dfe49b75cbf191523ec5b390073410f590
andrea8980 <andrea@casaloda.it>
2026-09-16 15:13:11 +0200

    Refactor DashboardController index method and update route to use root path

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return "index amministratore";
+        $user = Auth::user();
+        return Auth::check();
+       
     }
 
     public function profile()
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::middleware(['auth', 'verified'])
     ->name('admin.')
     ->prefix('admin')
     ->group(function () {
-        Route::get('/index', [DashboardController::class, 'index'])
+        Route::get('/', [DashboardController::class, 'index'])
         ->name('dashboard');
 
         Route::get('/profile', [DashboardController::class, 'profile'])
